f80184595d6c1c103ad3e1ce715eea4de5096cba
TristanBomb <TristanBomb@gmail.com>
2016-05-09 19:38:44 -0700

    Fixed semicolon in Person.h, added ideology data to Person.h

--- Person.h
+++ Person.h
@@ -22,4 +22,8 @@ public:
 	int age = 0;
 	int money = 0;
 	int happinessPercent = 0;
-}
+	signed char ideoEcononomy = 0; //Range is -3 (collectivist) to +3 (individualist)
+	signed char ideoPhilosophy = 0; //Range is -3 (rationalist) to +3 (spiritualist)
+	signed char ideoMilitary = 0; //Range is -3 (pacifist) to +3 (militarist)
+	signed char ideoGovernment = 0; //Range is -3 (democratic) to +3 (autocratic)
+};
